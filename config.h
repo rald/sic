bc623d7960ffb44e4339486ec46cd5056fb2208a
rald <thecoconutnutisagiantnut@gmail.com>
2023-03-01 10:23:25 +0800

    OK

---
+++ config.h
@@ -0,0 +1,14 @@
+/* Host used when "-h" is not given */
+#define DEFAULT_HOST "irc.undernet.org"
+
+/* Port used when "-p" is not given */
+#define DEFAULT_PORT "6667"
+
+/* Timestamp format; see strftime(3). */
+#define TIMESTAMP_FORMAT "%a%b%d %l:%M%P"
+
+/* Command prefix character. In most IRC clients this is '/'. */
+#define COMMAND_PREFIX_CHARACTER '/'
+
+/* Parting message used when none is specified with ":l ..." command. */
+#define DEFAULT_PARTING_MESSAGE "ang mamatay ng dahil sayo..."
